54a8410229329364df133f1ef71da1c198605738
Marc Sluiter <msluiter@redhat.com>
2019-12-11 14:35:38 +0100

    Support rt kernel updates, updated Readme (#35)
    and fix the actual installation of the RT kernel
    
    Signed-off-by: Marc Sluiter <msluiter@redhat.com>

--- features/performance/hack/generate.sh
+++ features/performance/hack/generate.sh
@@ -19,7 +19,9 @@ mkdir -p ${MANIFESTS_GENERATED_DIR}
     fi
 
     export RT_KERNEL_BASE64="$(base64 -w 0 ${ASSETS_DIR}/rt-kernel-patch.sh)"
-    
+    export MICROCODE_URL=${MICROCODE_URL:-http://file.rdu.redhat.com/~walters/microcode_ctl-20190918-3.rhcos.1.el8.x86_64.rpm}
+    export RT_REPO_URL=${RT_REPO_URL:-http://download-node-02.eng.bos.redhat.com/rhel-8/nightly/RHEL-8/latest-RHEL-8.1.1/compose/RT/x86_64/os}
+
     for template in $(ls ${TEMPLATES_DIR}); do
         name="$(basename ${template} .in)"
         envsubst < ${TEMPLATES_DIR}/${template} > ${MANIFESTS_GENERATED_DIR}/${name}
