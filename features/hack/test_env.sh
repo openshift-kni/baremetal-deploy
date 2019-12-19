10a8e258719d1ed0a5e4cfcce67256ff32279e1a
Yanir Quinn <yquinn@redhat.com>
2019-12-19 15:53:53 +0200

    Functional tests - label one node as worker-rt

--- features/hack/test_env.sh
+++ features/hack/test_env.sh
@@ -3,4 +3,7 @@
 # Performance
 export ISOLATED_CPUS=1
 export RESERVED_CPUS=0
-export NON_ISOLATED_CPUS=1
+export NON_ISOLATED_CPUS=0
+
+WORKER_NODE=$(oc get nodes --no-headers -o name -l node-role.kubernetes.io/worker="" | head -n1)
+oc label $WORKER_NODE node-role.kubernetes.io/worker-rt=""
