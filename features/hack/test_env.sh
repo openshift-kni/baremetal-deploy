cf390d79bf1d86a5d7bcc61827f0260d9b60c4cc
Sebastian Sch <sebassch@gmail.com>
2019-12-26 15:22:42 +0200

    Add the test_env for the PTP deployment
    This PR add the enviroment variables to run in the openshift-ci
    
    Signed-off-by: Sebastian Sch <sebassch@gmail.com>

--- features/hack/test_env.sh
+++ features/hack/test_env.sh
@@ -6,5 +6,11 @@ export RESERVED_CPUS=0
 export NON_ISOLATED_CPUS=0
 export HUGEPAGES_NUMBER=1
 
+# PTP
+export ptp_operator="quay.io/openshift/origin-ptp-operator"
+export ptp_daemon="quay.io/openshift/origin-ptp"
+export nic="eno2"
+export subscription_source="opsrctest"
+
 NON_MASTER_NODE=$(oc get nodes --no-headers -o name -l '!node-role.kubernetes.io/master' | head -n1)
 oc label --overwrite ${NON_MASTER_NODE} node-role.kubernetes.io/worker-rt=""
