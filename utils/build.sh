a681f1812e807807573264a3890c606157c20a04
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2020-05-07 08:17:32 +0200

    Prepare website and generation scripts to have different sections
    Signed-off-by: Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>

--- utils/build.sh
+++ utils/build.sh
@@ -2,18 +2,28 @@
 # Builds documentation for each release in both HTML and PDF versions
 
 # Space separated version list to build
-RELEASES="4.5 4.4 4.3"
+RELEASES="4.4 4.3"
+
+# Versioned documents
+DOCS="Deployment"
+
+# Documents using latest
+STATIC="Deployment"
+
+# Devel releases for static documents and devel docs
+DEVRELEASE="4.5"
 
 build_for_release() {
-    release="${1}"
+    doc="${1}"
+    release="${2}"
 
-    echo "Building documentation for release ${release}"
+    echo "Building documentation doc ${doc} for release ${release}"
     # Build the documentation
-    asciidoctor -a release=${release} -a toc=left -b xhtml5 -d book -B documentation/ documentation/index.adoc -D ../website/${release} 2>&1 | grep -v 'Try: gem'
+    asciidoctor -a release=${release} -a toc=left -b xhtml5 -d book -B documentation/ documentation/${doc}.adoc -D ../website/${release} 2>&1 | grep -v 'Try: gem'
     myrc=${?}
 
     # Build the documentation PDF
-    asciidoctor-pdf -a release=${release} -a toc=left -d book -B documentation/ documentation/index.adoc -D ../website/${release} 2>&1 | grep -v 'Try: gem'
+    asciidoctor-pdf -a release=${release} -a toc=left -d book -B documentation/ documentation/${doc}.adoc -D ../website/${release} 2>&1 | grep -v 'Try: gem'
 }
 
 # Define the index file
@@ -23,7 +33,16 @@ RC=0
 
 # Build all releases
 for release in ${RELEASES}; do
-    build_for_release ${release}
+    for doc in ${DOCS}; do
+        build_for_release "${doc}" "${release}"
+    done
+done
+
+# Build latest for static
+for release in ${DEVRELEASE}; do
+    for doc in ${STATIC}; do
+        build_for_release "${doc}" "${release}"
+    done
 done
 
 # Build JSON of generated documentation pages
@@ -35,11 +54,27 @@ mkdir -p ${TARGET}
 # Empty file before starting
 >${TARGET}/releases.yml
 for release in ${RELEASES}; do
-    echo """
-${release}:
-    name: ${release}
-    folder: ${release}/
+    for doc in ${DOCS}; do
+        echo """
+${doc}-${release}:
+    name: ${doc}
+    release: ${release}
+    folder: ${release}/${doc}
     """ >>${TARGET}/releases.yml
+    done
+done
+
+# Empty file before starting
+>${TARGET}/static.yml
+for release in ${DEVRELEASE}; do
+    for doc in ${STATIC}; do
+        echo """
+${doc}-${release}:
+    name: ${doc}
+    release: ${release}
+    folder: ${release}/${doc}
+    """ >>${TARGET}/static.yml
+    done
 done
 
 # TODO: CHECK why RC != 0 with no errors website (despite of gem)
