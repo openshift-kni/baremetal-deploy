2749540a9f652040d7d7968ba64bd1b039325f45
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2020-05-29 10:58:58 +0200

    Use headers in gulp index and add DEVPREV docs (#421)

--- utils/build-gulp.sh
+++ utils/build-gulp.sh
@@ -13,10 +13,15 @@ mystaticreleases=$(cat website/_data/static.yml | grep release | cut -d ":" -f 2
 declare -a mystaticdocs
 readarray -t mystaticdocs <<<$(cat website/_data/static.yml | grep name | cut -d ":" -f 2- | sort -u)
 
+mydevreleases=$(cat website/_data/devprev.yml | grep release | cut -d ":" -f 2- | sort -u -r)
+declare -a mydevprev
+readarray -t mydevprev <<<$(cat website/_data/devprev.yml | grep name | cut -d ":" -f 2- | sort -u)
+
 # Empty index
 >website/index.html
 
 # Versioned documents
+echo "<h2>Versioned</h2>" >>website/index.html
 echo "<ul>" >>website/index.html
 for release in ${myreleases}; do
     for doc in "${mydocs[@]}"; do
@@ -24,13 +29,24 @@ for release in ${myreleases}; do
         echo "<li><a href=\"${release}/${doc}\">${release}-${doc}</a></li>" >>website/index.html
     done
 done
-
 echo "</ul>" >>website/index.html
 
 # Static documents
+echo "<h2>Static</h2>" >>website/index.html
 echo "<ul>" >>website/index.html
 for release in ${mystaticreleases}; do
     for doc in "${mystaticdocs[@]}"; do
+        doc=$(echo "${doc}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+        echo "<li><a href=\"${release}/${doc}\">${release}-${doc}</a></li>" >>website/index.html
+    done
+done
+echo "</ul>" >>website/index.html
+
+# DEVEL documents
+echo "<h2>Development</h2>" >>website/index.html
+echo "<ul>" >>website/index.html
+for release in ${mydevreleases}; do
+    for doc in "${mydevprev[@]}"; do
         doc=$(echo "${doc}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
         echo "<li><a href=\"${release}/${doc}\">${release}-${doc}</a></li>" >>website/index.html
     done
