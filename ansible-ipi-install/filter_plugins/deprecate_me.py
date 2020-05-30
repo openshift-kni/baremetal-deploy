24527f38320669cd142348e39d34c01528f34658
Roger Lopez <rlopez@redhat.com>
2020-05-30 11:58:52 -0500

    Add the ability to include a warning message or deprecated message to any task

---
+++ ansible-ipi-install/filter_plugins/deprecate_me.py
@@ -0,0 +1,10 @@
+from ansible.utils.display import Display
+
+
+class FilterModule(object):
+    def filters(self): return {'deprecate_me': self.warn_filter}
+
+    def warn_filter(self, message, **kwargs):
+        Display().deprecated(message)
+        return message
+
