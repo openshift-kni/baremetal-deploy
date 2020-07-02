16638c5071a52766653abaa625ba58a2949c0fe6
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2020-07-02 14:28:49 +0200

    Add retries to URI
    Signed-off-by: Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>

--- ansible-ipi-install/filter_plugins/deprecate_me.py
+++ ansible-ipi-install/filter_plugins/deprecate_me.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
 from ansible.utils.display import Display
 
 
 class FilterModule(object):
-    def filters(self): return {'deprecate_me': self.warn_filter}
+    def filters(self):
+        return {"deprecate_me": self.warn_filter}
 
     def warn_filter(self, message, **kwargs):
         Display().deprecated(message)
         return message
-
--- ansible-ipi-install/filter_plugins/warn_me.py
+++ ansible-ipi-install/filter_plugins/warn_me.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
 from ansible.utils.display import Display
 
 
 class FilterModule(object):
-    def filters(self): return {'warn_me': self.warn_filter}
+    def filters(self):
+        return {"warn_me": self.warn_filter}
 
     def warn_filter(self, message, **kwargs):
         Display().warning(message)
