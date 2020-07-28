144a7ac326c89c9956cb8c16a5456c2937089d98
Jared O'Connell <46976761+jaredoconnell@users.noreply.github.com>
2020-07-28 14:27:28 -0400

    Auto-approve CSRs (#33)
    * Run an async script to auto-approve CSRs
    
    * Skip approval command when there is nothing to approve

---
+++ ansible-ipi-install/approve_csr.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Validate there being one arg
+if [ $# -ne 1 ]
+then
+    echo "There should be one arg: the number of workers expected."
+    exit 1
+fi
+
+# Loop until the expected number of workers are ready
+until [ $(oc get nodes | grep "\bReady\s*worker" | wc -l) == $1 ]
+do
+    # Search for CSRs awaiting approval
+    csr_output=$(oc get csr -ojson | jq -r '.items[] | select(.status == {} ) | .metadata.name')
+
+    # Don't run if empty
+    if [ -n "$csr_output" ]
+    then
+        # Approve
+    	echo "$csr_output" | xargs oc adm certificate approve
+    fi
+    sleep 5
+done
