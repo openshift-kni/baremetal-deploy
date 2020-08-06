730dbb3945956f719fd81c3b07c6733d627a19a9
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2020-08-06 15:45:45 +0200

    Sort versions for docs (#483)

--- utils/build.sh
+++ utils/build.sh
@@ -2,7 +2,7 @@
 # Builds documentation for each release in both HTML and PDF versions
 
 # Space separated version list to build
-RELEASES="4.4 4.3 4.5"
+RELEASES="4.5 4.4 4.3"
 
 # Devel releases for static documents and devel docs
 DEVRELEASE="4.6"
